75a1562b726779bcd12a0b9c1032096b6a2b1f5e
Umer Farooq <mumerfarooq557@gmail.com>
2024-12-13 11:52:09 +0500

    responsive navbar

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,40 +1,52 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     const navData = [
         {
             name: 'Summary',
             href: '/summary',
         },
         {
-            name: 'Add Transection',
+            name: 'Add Transaction',
             href: '/addTransactions',
         },
-
-    ]
+    ];
 
     return (
-        <div className="fixed z-50 w-5/6 text-xl font-bold  flex-none transition-colors mt-4 ">
+        <div className="fixed z-50 w-full text-xl font-bold flex-none transition-colors mt-4 bg-white">
             <div className="px-4 flex justify-between items-center">
                 <div>
                     <a href={'/'} className="font-jostBold text-4xl text-green hover:text-yellow">
                         Expense Tracker
                     </a>
-
                 </div>
-                <div className="flex justify-around">
-                    {navData.map((n, index) => {
-                        return (
-                            <Link to={n.href} key={index} className="mx-4 font-jostRegular text-2xl text-green hover:text-yellow hover:underline hover:underline-offset-8 transition">
-                                {n.name}
-                            </Link>
-                        )
-                    })}
+
+                <div className="flex md:hidden">
+                    <button
+                        className="text-4xl text-green hover:text-yellow"
+                        onClick={() => setIsMenuOpen(!isMenuOpen)}
+                    >
+                        ☰
+                    </button>
+                </div>
+
+                <div className={`flex flex-col md:flex-row md:justify-around ${isMenuOpen ? 'block' : 'hidden'} md:block`}>
+                    {navData.map((n, index) => (
+                        <Link
+                            to={n.href}
+                            key={index}
+                            className="mx-4 font-jostRegular text-2xl text-green hover:text-yellow hover:underline hover:underline-offset-8 transition"
+                        >
+                            {n.name}
+                        </Link>
+                    ))}
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
